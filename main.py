226a396c1985ffece241072da49718a6a11ea27c
Your Name <you@example.com>
2024-02-21 10:17:41 +0200

    pok

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ with open('pokemon.csv', newline='') as csv_file:
 
     for row in file_reader:
         pokemons.append(row[0])
-
-# print(pokemons)
+print(pokemons)
 
 print("Pokemon list command:")
 
@@ -27,19 +26,38 @@ while True:
 
     if choice == '1':
         # https://www.w3schools.com/python/python_lists_access.asp
+        pokemonsindex = input()
+        print("you choise" + pokemonsindex)
+        print(pokemons[int(pokemonsindex)])
         pass
     elif choice == '2':
         # https://www.w3schools.com/python/python_lists_sort.asp
+        pokemons.sort()
+        print(pokemons)
         pass
     elif choice == '3':
         # https://www.w3schools.com/python/python_lists_sort.asp
+        pokemons.sort(reverse=True)
+        print(pokemons)
         pass
     elif choice == '4':
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/ref_string_startswith.asp
-        pass
+        newlist =[]
+        name=input()
+        for x in pokemons:
+          if name in x:
+              newlist.append(x)
+        print(newlist)
     elif choice == '5':
         # https://www.w3schools.com/python/python_lists_comprehension.asp
+        newlist =[]
+        length=int(input())
+        for pokemon in pokemons:
+          if len(pokemon) == length:
+              newlist.append(pokemon)
+        print(newlist)
+
         pass
     elif choice == '6':
         print("Exiting")
